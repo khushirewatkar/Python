7eedd3c57085d94c69be5b3836c18913efd5845d
khushirewatkar <167541098+khushirewatkar@users.noreply.github.com>
2026-07-30 22:42:45 +0530

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,112 @@
+# a="khushi"
+# b= 10
+# c =20.2
+# d= True
+# e= [1,2,3]
+# f=(1,2,3)
+# g={1,2,3}
+# h={"name":"Khushi"}
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+
+
+# name= input("enter your name:")
+# age=int(input("enter ypur age:"))
+# cgpa = float(input("enter your cgpa:"))
+# print(name + "is " +str(age)+ "year old and having" + str(cgpa)+ "cgpa")
+# # "+" se sirf strings hi connect kiye jaa sakte haii isiliye age and cgpa(int and float)ko string me convert kiya
+
+# print("assignment")
+# a=10
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# # float value k liye /
+# print(a/b) 
+# # int value k liye //
+# print(a//b)
+# print(a**b)
+# print(a%b)
+
+# print("comparison")
+# print(a>b)
+# print(a<b)
+# print(a==b)
+# print(a!=b)
+
+# print("Logical")
+# x=True
+# y=False
+# print(x and y)
+# print(x or y)
+# print(not x)
+# print(not y)
+
+# print("assignment: ")
+# x=5
+# print(x)
+# x+=2
+# print(x)
+# x *=2
+# print(x)
+
+# x=10
+# y=5
+# print(x + y  * 2)
+# # output : 20(according to BODMAS)
+
+# print(type(10.4))
+
+# print(2**3)
+
+
+# # 2
+# a=int (input("Enter A:"))
+# b=int (input("Enter B:"))
+# print("Addition: "+str(a+b))
+# print("Substraction: "+str(a-b))
+# print("Multiplication: "+str(a*b))
+# print("Division: "+str(a/b))
+
+# 3
+# r= int(input("Enter Radius: "))
+# pi=3.14
+# area1= pi*r**2
+# area2= pi*r*r
+# print(area1)
+# print(area2)
+
+# a=10
+# b=20
+# temp=a
+# a=b
+# b=temp
+# print(a)
+# print(b)
+
+# a=10
+# b=20
+# a,b=b,a
+# print(a)
+# print(b)
+
+# a= int (input("enter temp in C:"))
+# F = (a * 9/5) + 32
+# print("temp in fahrenheit is "+ str(F))
+
+k=input("enter name: ")
+h=int(input("enter age: "))
+u=float(input("enter height: "))
+s=int(input("enter weight: "))
+BMI = s / (u * u)
+print("HELLO "+ k)
+print("Your BMI IS "+ str(BMI))
+print("thank you!!")
