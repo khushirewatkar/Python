00d857de1f29fa09ea1732a65db068d0c264d74e
khushirewatkar <167541098+khushirewatkar@users.noreply.github.com>
2026-07-31 22:58:56 +0530

    Add files via upload

---
+++ d2.py
@@ -0,0 +1,106 @@
+# if elif else
+# m= int(input("enter the marks: "))
+# if m>=90:
+#     print("A grade")
+# elif m>80 and m<90:
+#     print("B grade")
+# elif m>70 and m<80:
+#     print("C grade")
+# else:
+#     print("can do better")
+
+#  Nested if 
+# age= int(input("Enter your age: "))
+# c= input("are you indian(y/n)?")
+# if age>= 18:
+#     if c =="y":
+#         print("Eligible")
+#     else:
+#         print("Not eligible")
+#         print("due to not indian citizen")
+# else:
+#     print("Underage")
+
+# log_in= True
+# if not log_in:
+#     print("please login")
+# else: 
+#     print("you are loged in")
+
+
+# Match day
+# d = int(input("enter day: "))
+# match d:
+#     case 1:
+#         print("monday")
+#     case 2:
+#         print("tues")
+#     case 3:
+#         print("wed")
+#     case _ :
+#         print("another")
+
+# q1
+# x=5
+# if x>10:
+#     print("A")
+# else:
+#     print("B")
+
+# q2
+# age =20
+# if age >18:
+#     print("adult")
+# elif age>15:
+#     print("teen")
+# else:
+#     print("child")
+
+# q3
+# print(15%2==0)
+
+# q4
+# x=10
+# if x:
+#     print("hello")
+
+# # q5
+# x=0.5
+# if x:
+#     # ye tabhi print hoga jab x ki value 0.5 k upar hogii
+#     print("hi")
+# else:
+#     # ye 0.5 k niche wali values hogi tab print hoga
+#     print("bye")
+
+# y =int(input("enter year: "))
+# if (y % 400 == 0) or (y % 4 == 0 and y % 100 !=0):
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+# char = input("enter any one character(a-z): ")
+# if char==("a" or "e" or "i" or "o" or "u"):
+#     print("this chracter is vowel")
+# else:
+#     print("not a vowel")
+
+# user="python123"
+# passwd="admin"
+# inr=input("enter username :")
+# ins=input("enter password: ")
+# if inr==user and ins== passwd:
+#     print("login success")
+# else:
+#     print("fail")
+
+# choice= int(input("enter choice: "))
+# match choice:
+#     case 1:
+#         print("check balance")
+#     case 2:
+#         print("deposite")
+#     case 3:
+#         print("cash withdrawl")
+#     case _:
+#         print("exit")
